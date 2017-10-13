625cd7e0d03aac660b3255b4eb09f9f0e0eb7237
Harrison <->
2017-10-13 17:03:53 -0500

    added .gitignore and config.json

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# weatherapp" 
+"# weatherapp
+
+There is a .config that has the api key for openweathermap.org.
+
+config.json : 
+{
+	"APPID" : "Your APPID"
+} 
+"
---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready({
+
+});
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position){
+    	latitude = position.coords.latitude
+    	longitude = position.coords.longitude
+    	console.log("Latitude: " + latitude)
+	   	console.log("Longitude: " + longitude)
+	   	// console.log("Location: " +  json)
+    	$.getJSON("https://raw.githubusercontent.com/Harrison97/data/master/city.list.json", function(json){
+    		var city = json[0];
+    		console.log("Getting city...")
+    		for (var i = 1; i < json.length; i++) {
+    			if(Math.abs(latitude - json[i].coord.lat) + Math.abs(longitude - json[i].coord.lon) < Math.abs(latitude - city.coord.lat) + Math.abs(longitude - city.coord.lon)) {
+    				city = json[i]
+    			}
+    		}
+    		console.log(city)
+    	})
+	   	
+    });
+} else {
+	console.log("Location not found!")
+}
