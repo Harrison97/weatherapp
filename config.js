42c498e44ed50985fe43df54ab676a7da07b6005
Harrison <->
2017-10-14 03:11:33 -0500

    Finnished. Added bg gif. full desired functionality. background darkness related to local time.

--- config.js
+++
@@ -1,4 +0,0 @@
-//config
-module.exports = {
-	apikey : "e32de2e906deb7dd12f70b72c186b8aa"
-}
--- index.js
+++ index.js
@@ -1,6 +1,52 @@
-import apikey from 'config.js'
-$(document).ready({
+var date = new Date();
+var hour = date.getHours();
+var opacity = 0;
+switch(hour) {
+    case 0:
+    case 1:
+    case 3:
+        opacity = .1;
+        break;
+    case 4:
+    case 5:
+        opacity = .4;
+        break
+    case 6:
+    case 7:
+        opacity = .7;
+        break;
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+        opacity = 1;
+        break;
+    case 17:
+    case 18:
+        opacity = .8;
+        break;
+    case 19:
+    case 20:
+        opacity = .4;
+        break;
+    case 20:
+    case 21:
+        opacity = .3;
+        break;
+    case 22:
+    case 23:
+        opacity = .1;
+        break;
+    default:
+        opacity = 1;
+}
 
+$(document).ready(function(){
+    $("#bg-img").animate({opacity: opacity}, 2*1000);
 });
 
 if (navigator.geolocation) {
@@ -8,26 +54,35 @@ if (navigator.geolocation) {
     	latitude = position.coords.latitude
     	longitude = position.coords.longitude
     	console.log("Latitude: " + latitude)
-	   	console.log("Longitude: " + longitude)
-	   	// console.log("Location: " +  json)
-    	$.getJSON("https://raw.githubusercontent.com/Harrison97/data/master/city.list.json", function(json){
-    		var city = json[0];
-    		console.log("Getting city...")
-    		for (var i = 1; i < json.length; i++) {
-    			if(Math.abs(latitude - json[i].coord.lat) + Math.abs(longitude - json[i].coord.lon) < Math.abs(latitude - city.coord.lat) + Math.abs(longitude - city.coord.lon)) {
-    				city = json[i]
-    			}
-    		}
-    		console.log(city)
-    		
-    		$.getJSON("https://api.darksky.net/forecast/" + config.apikey + "/" + latitude + "," + latitude, function(weather){
-    			console.log(weather)
-    		})
-    		
-    		
+	   	console.log("Longitude: " + longitude)		
 
-    	})
-	   	
+        var url = "https://fcc-weather-api.glitch.me/api/current?lat="+latitude+"&lon="+longitude
+        $.getJSON(url, function(city){
+            console.log(city);
+            $("#city-name").html(city.name)
+            $("#city-temp").html(city.main.temp + "°C")
+            $("#city-humidity").html(city.main.humidity + "% Humidity")
+            $("#city-weather").html(city.weather[0].main)
+            var icon=""
+            switch(city.weather[0].main) {
+                case "Rain":
+                    icon = "rain"
+                    break;
+                case "Clear":
+                    icon = "cloud"
+                    break;
+                case "Clouds":
+                    icon = "cloud"
+                    break;
+                case "Snow":
+                    icon = "snow"
+                    break;
+                default:
+                    icon = "cloud"
+            }
+            $("#weather-icon").attr("class", "wi wi-" + icon)
+        })
+
     });
 } else {
 	console.log("Location not found!")
